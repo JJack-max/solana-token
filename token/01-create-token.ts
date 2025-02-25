6152eee0d0cc1b81ecd0e010e17ef6686c76415c
root <root@DESKTOP-8QHEPA3>
2025-02-26 08:56:10 +0900

    ss

--- token/01-create-token.ts
+++ token/01-create-token.ts
@@ -1,6 +1,7 @@
 import { createMint } from "@solana/spl-token";
 import { getConnection, getWallet } from "../common/wallet";
 import { log } from "../common/my-log";
+import { Connection, Keypair } from "@solana/web3.js";
 
 (async () => {
 
@@ -9,6 +10,14 @@ import { log } from "../common/my-log";
   let payer = getWallet("logs/payer-DhNniw9HjpYKbvNtTiyZ225CujNr4HpByn21SNysm7n7.json");
   let mintAuthority = getWallet("logs/mintAuthority-6VGigX1ScxmrMcQ1UjRZvboWZ81FRx6pV1a5WADTE4Ya.json");
   let freezeAuthority = getWallet("logs/freezeAuthority-BLv3MfnQCtqnzFT95WQE2GtQopjMF1Cgf6RgPtpXz42j.json");
+
+  create1(connection, payer, mintAuthority, freezeAuthority);
+
+
+})();
+
+
+async function create1(connection: Connection, payer: Keypair, mintAuthority: Keypair, freezeAuthority: Keypair) {
 
   let res = await createMint(
     connection,
@@ -19,7 +28,9 @@ import { log } from "../common/my-log";
   );
 
   log("token", res.toString());
+}
+
+async function create2(connection: Connection, payer: Keypair, mintAuthority: Keypair, freezeAuthority: Keypair) {
 
 
-
-})();
+}
