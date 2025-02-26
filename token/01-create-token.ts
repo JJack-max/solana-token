98e71c90c1ba32dfc0297aabf778c780ad8ca55f
root <root@DESKTOP-8QHEPA3>
2025-02-27 08:41:08 +0900

    ss

--- token/01-create-token.ts
+++ token/01-create-token.ts
@@ -1,7 +1,7 @@
-import { createMint } from "@solana/spl-token";
+import { createInitializeMintInstruction, createMint, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { getConnection, getWallet } from "../common/wallet";
 import { log } from "../common/my-log";
-import { Connection, Keypair } from "@solana/web3.js";
+import { Connection, Keypair, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
 
 (async () => {
 
@@ -11,7 +11,8 @@ import { Connection, Keypair } from "@solana/web3.js";
   let mintAuthority = getWallet("logs/mintAuthority-6VGigX1ScxmrMcQ1UjRZvboWZ81FRx6pV1a5WADTE4Ya.json");
   let freezeAuthority = getWallet("logs/freezeAuthority-BLv3MfnQCtqnzFT95WQE2GtQopjMF1Cgf6RgPtpXz42j.json");
 
-  create1(connection, payer, mintAuthority, freezeAuthority);
+  // create1(connection, payer, mintAuthority, freezeAuthority);
+  create2(connection, payer, mintAuthority, freezeAuthority);
 
 
 })();
@@ -32,5 +33,27 @@ async function create1(connection: Connection, payer: Keypair, mintAuthority: Ke
 
 async function create2(connection: Connection, payer: Keypair, mintAuthority: Keypair, freezeAuthority: Keypair) {
 
+  const mint = Keypair.generate();
 
+  const tx = new Transaction().add(
+    SystemProgram.createAccount({
+      fromPubkey: payer.publicKey,
+      newAccountPubkey: mint.publicKey,
+      space: MINT_SIZE,
+      lamports: await getMinimumBalanceForRentExemptMint(connection),
+      programId: TOKEN_PROGRAM_ID,
+    }),
+    createInitializeMintInstruction(
+      mint.publicKey,
+      8,
+      mintAuthority.publicKey,
+      freezeAuthority.publicKey,
+    ),
+  );
+
+  let signature = await sendAndConfirmTransaction(connection, tx, [payer, mint]);
+
+  log("token", mint.publicKey.toString());
+
+  console.log(signature);
 }
