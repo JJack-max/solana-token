7b6b209cb57b21446c17477ca44e8641c6b99d0f
root <root@DESKTOP-SS97LUH.>
2025-03-09 20:28:27 +0900

    ss

--- ar/airdrop-ar.ts
+++ ar/airdrop-ar.ts
@@ -1,14 +1,17 @@
+import Arweave from "arweave";
 import { readFileSync } from "fs";
 import { getArweave } from "../common/arweave";
 
 (async () => {
     let arweave = getArweave();
 
-    let walletStr: string = "logs/ar-Jack-4Sj84EvOlo317Sk0YjlR7uI3IJ_R7uwcjsvHPiJSwN4.log";
+})();
 
-    const wallet = JSON.parse(readFileSync(walletStr, "utf-8"))
+async function airdrop(arweave: Arweave, wallet: string, num: number) {
+
+    const wallett = JSON.parse(readFileSync(wallet, "utf-8"))
     // const wallet = await arweave.wallets.generate();
-    const addr = await arweave.wallets.getAddress(wallet);
+    const addr = await arweave.wallets.getAddress(wallett);
     let res = await arweave.api.get(`mint/${addr}/10000000000000000`);
     console.log(`airdrop result:${JSON.stringify(res)}`);
-})();
+}
