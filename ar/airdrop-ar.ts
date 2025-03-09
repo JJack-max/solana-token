444d8028920d8e11d5f6950aa0c814ddec64c6da
root <root@DESKTOP-SS97LUH.>
2025-03-09 20:34:50 +0900

    ss

--- ar/airdrop-ar.ts
+++ ar/airdrop-ar.ts
@@ -1,17 +1,19 @@
 import Arweave from "arweave";
 import { readFileSync } from "fs";
 import { getArweave } from "../common/arweave";
+import { JWKInterface } from "arweave/node/lib/wallet";
 
 (async () => {
     let arweave = getArweave();
-    airdrop(arweave, "logs/ar-Jack-4Sj84EvOlo317Sk0YjlR7uI3IJ_R7uwcjsvHPiJSwN4.log", 1000);
+    // airdrop(arweave, "logs/ar-Jack-4Sj84EvOlo317Sk0YjlR7uI3IJ_R7uwcjsvHPiJSwN4.log", 1000);
+    const wallet = JSON.parse(readFileSync("logs/ar-Jack-4Sj84EvOlo317Sk0YjlR7uI3IJ_R7uwcjsvHPiJSwN4.log", "utf-8"))
+
 })();
 
-async function airdrop(arweave: Arweave, wallet: string, num: number) {
+async function airdrop(arweave: Arweave, wallet: JWKInterface, num: number) {
 
-    const wallett = JSON.parse(readFileSync(wallet, "utf-8"))
     // const wallet = await arweave.wallets.generate();
-    const addr = await arweave.wallets.getAddress(wallett);
+    const addr = await arweave.wallets.getAddress(wallet);
     let res = await arweave.api.get(`mint/${addr}/10000000000000000`);
     console.log(`airdrop result:${JSON.stringify(res)}`);
 }
